d83aa6459e594bad12e41b1448ea5d790f119ff1
Daisuke Tsuji <dim0627@gmail.com>
2021-06-09 08:12:18 +0900

    split to two col.

---
+++ components/DdlForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Row, Col } from 'react-grid-system';
+
+const Component = () => (
+  <Row>
+    <Col sm={6}>
+      <h2>Table Information Import From</h2>
+    </Col>
+    <Col sm={6}>
+      <h2>DDL for Make View Table</h2>
+    </Col>
+  </Row>
+);
+
+export default Component;
