4e453bf1934f12f27708c33c054ebc023a9bf0db
Daisuke Tsuji <dim0627@gmail.com>
2021-06-09 08:10:16 +0900

    fixing a resolve tsx file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,22 @@ module.exports = {
   rules: {
     'no-use-before-define': 'off',
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never', jsx: 'never', ts: 'never', tsx: 'never',
+    }],
     'react/jsx-props-no-spreading': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+      typescript: {},
+    },
   },
 };
